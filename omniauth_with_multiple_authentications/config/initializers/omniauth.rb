1bbc8ee7d00358f175e47342362dddf3148911e6
Edijs Petersons <eddpeterson@gmail.com>
2011-12-27 20:57:33 +0100

    Allowing already signed in user through devise to add multiple authentications e.g. Twitter, Facebook

---
+++ omniauth_with_multiple_authentications/config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :facebook, 'appid', 'secret', 
+    {:scope => 'offline_access,email,user_birthday', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  
+end
--- omniauth_with_multiple_authentications/config/routes.rb
+++ omniauth_with_multiple_authentications/config/routes.rb
@@ -1,4 +1,8 @@
 OmniauthWithMultipleAuthentications::Application.routes.draw do
+  resources :authentications
+
+  match '/auth/:provider/callback' => 'authentications#create'
+  
   devise_for :users do
     get "sign_in", :to => "devise/sessions#new", :as => 'signin'
     get "sign_out", :to => "devise/sessions#destroy", :as => 'signout'
